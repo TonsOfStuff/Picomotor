feb980b0e330f2859d7bd799f54ca4b56652a98d
Jerry Du <jtons2008@gmail.com>
2026-08-28 10:22:11 -0500

    Removed usb code that I don't think does anything. Will check but as of now makes everything more confusing

--- pylibpico.py
+++ pylibpico.py
@@ -1,32 +1,10 @@
 import time
 from pylablib.devices import Newport
-import usb.core
-import usb.util
 
 stage = None
+print(Newport.get_usb_devices_number_picomotor())
 try:
     print("Devices found:", Newport.get_usb_devices_number_picomotor())
-    
-    # Initialize connection
-    dev = usb.core.find(idVendor=0x104d)
-    if dev is not None:
-        try:
-            # Clear STALL/HALT state on all endpoint buffers
-            for config in dev:
-                for intf in config:
-                    for ep in intf:
-                        try:
-                            dev.clear_halt(ep.bEndpointAddress)
-                        except Exception:
-                            pass
-            # Force USB bus re-enumeration
-            dev.reset()
-            usb.util.dispose_resources(dev)
-            
-            # CRITICAL: Allow 1.2s for Windows driver to re-bind after reset
-            time.sleep(1.2)
-        except Exception:
-            pass
 
     stage = Newport.Picomotor8742(conn=0)
     
@@ -37,7 +15,11 @@ try:
     print("Master Controller ID:", stage.query("*IDN?", addr=1))
     print("Slave Controller ID:",  stage.query("*IDN?", addr=2))
 
-    print(stage.get_addr_map())
+    stage.move_by(1, -200, 1)
+    stage.wait_move(1, 1)
+
+    print(stage.get_position(1, 1))
+
 
 
 except Exception as e:
