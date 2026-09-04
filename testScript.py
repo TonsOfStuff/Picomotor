69073ab6370c2fd0d6eb98df7407b94fe5655e24
Jerry Du <jtons2008@gmail.com>
2026-09-04 09:09:44 -0500

    Refined test script in case anyone wants to ditch the GUI and write code to machine directly for more control

--- testScript.py
+++ testScript.py
@@ -1,64 +1,41 @@
+#Example code for using Newport Picomotor 8742 with multiple controllers
+
 import time
 from pylablib.devices import Newport
 import traceback;
 
 stage = None
-#Tripod angle, with 3 points on stage that tilt it up and down. So move all 3 to achieve up/down movement
 axesMap = {
-    "x": (1, 1), #Axis, Address  Y1
-    "y": (2, 1), #               Y2               
+    "y1": (1, 1), #Axis, Address  Y1
+    "y2": (2, 1), #               Y2               
     "z": (3, 2), #After testing  Z
-    "pitch": (1, 2), #           X1
-    "yaw": (2, 2) #              X2
+    "x1": (1, 2), #           X1
+    "x2": (2, 2) #              X2
 }
 try:
     print("Devices found:", Newport.get_usb_devices_number_picomotor())
 
-    stage = Newport.Picomotor8742(conn=0, multiaddr=True)
-    print(stage.get_addr_map());
+    stage = Newport.Picomotor8742(conn=0)
     
     # Target specific controllers directly
     print("Master Controller ID:", stage.query("*IDN?", addr=1))
     print("Slave Controller ID:",  stage.query("*IDN?", addr=2))
 
-    '''
-    axis, addr = axesMap["x"]
-    stage.move_by(axis, 200, addr)
-    stage.wait_move(axis, addr)
+    #Example move and wait move
+    #SIMULTANEOUS MOVEMENT ONLY WORKS IF ON TWO DIFFERENT CONTROLLERS (i.e different addresses)
+    #Otherwise must wait_move after a move command
+    stage.move_by(axis=1, steps=200, addr=1)
+    stage.move_by(axis=1, steps=200, addr=2)
 
-    axis, addr = axesMap["y"]
-    stage.move_by(axis, 200, addr)
-    stage.wait_move(axis, addr)
-
-    axis, addr = axesMap["z"]
-    
-    stage.move_by(axis=axis, steps=200, addr=addr)
-    print(stage.is_moving(axis, addr))
-    stage.wait_move(axis, addr)
-    print(stage.is_moving(axis, addr))
-    
-    axis, addr = axesMap["pitch"]
-    stage.move_by(axis, 200, addr)
-    stage.wait_move(axis, addr)
-    '''
-    print(stage.get_position(1, 1))
-    print(stage.get_position(2, 1))
-    print(stage.get_position(3, 2))
-    print(stage.get_position(1, 2))
-    print(stage.get_position(2, 2))
-    
-    stage.move_by(1, 200, 1)
-    stage.move_by(1, 200, 2)
-
-    stage.wait_move(1,1)
-    stage.wait_move(1, 2)
+    stage.wait_move(axis=1, addr=1)
+    stage.wait_move(axis=1, addr=2)
 
     print("After moving:")
-    print(stage.get_position(1, 1))
-    print(stage.get_position(2, 1))
-    print(stage.get_position(3, 2))
-    print(stage.get_position(1, 2))
-    print(stage.get_position(2, 2))
+    print(stage.get_position(axis=1, addr=1))
+    print(stage.get_position(axis=2, addr=1))
+    print(stage.get_position(axis=3, addr=2))
+    print(stage.get_position(axis=1, addr=2))
+    print(stage.get_position(axis=2, addr=2))
 
 
 
